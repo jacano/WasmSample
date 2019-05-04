6dac2f639985148677a33ff260085fcad0a2987e
jacano <jacanosalado@gmail.com>
2019-05-04 02:47:20 +0200

    initial commit

---
+++ src/sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WasmSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello wasm!");
+        }
+    }
+}
